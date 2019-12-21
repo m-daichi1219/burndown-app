aa5285da1b253ff040daa3a8a481fa68ee8b3b9f
m-daichi1219 <matu9525@gmail.com>
2019-12-21 21:48:57 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './component/App';
 import * as serviceWorker from './serviceWorker';
+import App2 from './setupTests';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
