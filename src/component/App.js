f4f9bb90717385887c1d817e9d44380de46a2605
m-daichi1219 <matu9525@gmail.com>
2019-12-27 23:17:12 +0900

    Add css to task area

--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import AppTableRow from './AppTableRow';
 import { ADD_TASK } from '../constants/action-types';
+import '../css/App.css';
 
 const App = () => {
   const rowDatas = useSelector((state) => state.datas);
@@ -13,8 +14,10 @@ const App = () => {
 
   return (
     <div className="App">
-      {rowDatas.map((rowData) => <AppTableRow id="tablerow" key={rowData.id} task={rowData} />)}
-      <button type="button" name="add-button" onClick={addTask}>タスクを追加</button>
+      <div className="task-area">
+        {rowDatas.map((rowData) => <AppTableRow id="tablerow" key={rowData.id} task={rowData} />)}
+        <button type="button" name="add-button" onClick={addTask}>タスクを追加</button>
+      </div>
     </div>
   );
 };
