18124d7760bdffc4c5ac7d497f3e79c30c3ad198
m-daichi1219 <matu9525@gmail.com>
2019-12-30 15:48:45 +0900

    Fix due to data model change

--- src/component/AppTableRow.js
+++ src/component/AppTableRow.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { EDIT_TASK, DELETE_TASK } from '../constants/action-types';
 import '../css/AppTableRow.css';
 
 const AppTableRow = (props) => {
   const { task } = props;
+  const sprints = useSelector((state) => state.sprints);
   const dispatch = useDispatch();
 
   const handleChange = (event) => {
@@ -31,7 +32,11 @@ const AppTableRow = (props) => {
         <input type="number" id="point" value={task.point} onChange={handleChange} />
       </div>
       <div className="cell">
-        <input type="date" id="endDate" value={task.endDate} onChange={handleChange} />
+        {/* <input type="date" id="endDate" value={task.endDate} onChange={handleChange} /> */}
+        <select id="sprint" onChange={handleChange} value={task.sprint}>
+          <option value=""> </option>
+          {sprints.map((sprint, index) => (<option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
+        </select>
       </div>
       <button type="button" onClick={handleDelete}>削除</button>
     </div>
@@ -48,7 +53,7 @@ AppTableRow.propTypes = {
   task: PropTypes.shape({
     title: PropTypes.string.isRequired,
     point: PropTypes.string.isRequired,
-    endDate: PropTypes.string.isRequired,
+    sprint: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
   }).isRequired,
 };
