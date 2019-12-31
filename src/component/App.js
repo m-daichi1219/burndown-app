754cf40bb66e49f499f5f4c13f82d739576a6f62
m-daichi1219 <matu9525@gmail.com>
2019-12-31 20:36:10 +0900

    Update Change configuration of AppTaskTable

--- src/component/App.js
+++ src/component/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import AppTaskTable from './AppTaskTable';
 import AppSprintTable from './AppSprintTable';
 import AppLineChart from './AppLineChart';
-import { ADD_TASK } from '../constants/action-types';
 import '../css/App.css';
 
-const App = () => {
-  const rowDatas = useSelector((state) => state.datas);
-  const dispatch = useDispatch();
-
-  const addTask = () => {
-    dispatch({ type: ADD_TASK });
-  };
-
-  return (
-    <div className="App">
-      <div className="task-area">
-        {rowDatas.map((rowData) => <AppTaskTable id="tablerow" key={rowData.id} task={rowData} />)}
-        <button type="button" name="add-button" onClick={addTask}>タスクを追加</button>
-      </div>
-      <div className="chart-area">
-        <AppLineChart />
-      </div>
-      <div className="sprint-area">
-        <AppSprintTable />
-      </div>
+const App = () => (
+  <div className="App">
+    <div className="task-area">
+      {/* {rowDatas.map((rowData) => <AppTaskTable id="tablerow" key={rowData.id} task={rowData} />)} */}
+      <AppTaskTable />
     </div>
-  );
-};
+    <div className="chart-area">
+      <AppLineChart />
+    </div>
+    <div className="sprint-area">
+      <AppSprintTable />
+    </div>
+  </div>
+);
+
 
 export default App;
--- src/component/AppTaskTable.js
+++ src/component/AppTaskTable.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
-import { EDIT_TASK, DELETE_TASK } from '../constants/action-types';
+import { ADD_TASK, EDIT_TASK, DELETE_TASK } from '../constants/action-types';
 import '../css/AppTaskTable.css';
 
-const AppTaskTable = (props) => {
-  const { task } = props;
+const AppTaskTable = () => {
+  const tasks = useSelector((state) => state.datas);
   const sprints = useSelector((state) => state.sprints);
   const dispatch = useDispatch();
 
@@ -23,32 +22,49 @@ const AppTaskTable = (props) => {
     dispatch({ type: DELETE_TASK, payload });
   };
 
-  return (
-    <div draggable="true" className="app-task-table" id={task.id}>
-      <div className="cell">
-        <input type="text" id="title" value={task.title} onChange={handleChange} />
-      </div>
-      <div className="cell">
-        <input type="number" id="point" value={task.point} onChange={handleChange} />
-      </div>
-      <div className="cell">
-        <select id="sprint" onChange={handleChange} value={task.sprint}>
-          <option value=""> </option>
-          {sprints.map((sprint, index) => (<option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
-        </select>
-      </div>
-      <button type="button" onClick={handleDelete}>削除</button>
-    </div>
-  );
-};
+  const handleClick = () => {
+    dispatch({ type: ADD_TASK });
+  };
 
-AppTaskTable.propTypes = {
-  task: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    point: PropTypes.string.isRequired,
-    sprint: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-  }).isRequired,
+
+  return (
+    <>
+      <div className="app-task-table">
+        <div className="app-table-row">
+          <div className="cell">
+            タスク名
+          </div>
+          <div className="cell">
+            ポイント
+          </div>
+          <div className="cell">
+            完了Sprint
+          </div>
+          <div className="cell">
+            削除
+          </div>
+        </div>
+        {tasks.map((task) => (
+          <div draggable="true" className="app-table-row" id={task.id} key={task.id}>
+            <div className="cell">
+              <input type="text" id="title" value={task.title} onChange={handleChange} />
+            </div>
+            <div className="cell">
+              <input type="number" id="point" value={task.point} onChange={handleChange} />
+            </div>
+            <div className="cell">
+              <select id="sprint" onChange={handleChange} value={task.sprint}>
+                <option value=""> </option>
+                {sprints.map((sprint, index) => (<option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
+              </select>
+            </div>
+            <button type="button" onClick={handleDelete}>削除</button>
+          </div>
+        ))}
+      </div>
+      <button type="button" name="add-button" onClick={handleClick}>タスクを追加</button>
+    </>
+  );
 };
 
 export default AppTaskTable;
