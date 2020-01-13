f21f11f7e355cffeb89d66883447c36256bf4ef1
m-daichi1219 <matu9525@gmail.com>
2020-01-13 16:18:48 +0900

    Add task board page components

--- src/component/AppTaskBoardView.js
+++ src/component/AppTaskBoardView.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Draggable from 'react-draggable';
 import '../css/AppTaskBoardView.css';
 
-const AppTaskBoardView = () => (
-  <div className="task-board">
-    <p>AppTaskBoardView</p>
-  </div>
-);
+const AppTaskBoardView = () => {
+  const tasks = useSelector((state) => state.datas);
+
+  // const onStart = (e, data) => {
+  //   console.log('onStart!');
+  //   console.log(e);
+  //   console.log(data);
+  // };
+
+  // const onDrag = (e, data) => {
+  //   console.log('onDrag!');
+  //   console.log(e);
+  //   console.log(data);
+  // };
+
+  // const onStop = (e, data) => {
+  //   console.log('onStop!');
+  //   console.log(e);
+  //   console.log(data);
+  // };
+
+  return (
+    <div className="task-board">
+      {tasks.map((task) => (
+        <Draggable>
+          <div key={task.id} className={task.tag}>
+            {task.title}
+          </div>
+        </Draggable>
+      ))}
+    </div>
+  );
+};
 
 export default AppTaskBoardView;
