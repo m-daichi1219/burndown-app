21e6137c1d7c1f33e9a778aac894e953fa78d128
hibohiboo <swdice66@gmail.com>
2020-01-24 20:00:19 +0900

    認証の実装

---
+++ src/firebase/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  apiKey: process.env.REACT_APP_FIREBSE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBSE_AUTH_DOMAIN,
+  rojectId: process.env.REACT_APP_FIREBSE_PROJECT_ID,
+};
