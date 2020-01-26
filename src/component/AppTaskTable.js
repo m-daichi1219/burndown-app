c37f946d19ad33c89dbdad6f2f4b2819b5eb0d25
m-daichi1219 <matu9525@gmail.com>
2020-01-26 14:43:27 +0900

    Fix task table layout

--- src/component/AppTaskTable.js
+++ src/component/AppTaskTable.js
@@ -30,54 +30,89 @@ const AppTaskTable = () => {
   return (
     <>
       <div className="app-task-table">
-        <div className="app-table-header">
-          <div className="task-cell">
-            タスク名
-          </div>
-          <div className="task-cell">
-            ポイント
-          </div>
-          <div className="task-cell">
-            完了Sprint
-          </div>
-          <div className="task-cell">
-            色
-          </div>
-          <div className="task-cell">
-            削除
-          </div>
-        </div>
-        <div className="app-table-body">
-          {tasks.map((task) => (
-            <div draggable="true" className="app-table-row" id={task.id} key={task.id}>
-              <div className="task-name-cell">
-                <input type="text" id="title" value={task.title} onChange={handleChange} />
-              </div>
-              <div className="task-point-cell">
-                <input type="number" id="point" value={task.point} onChange={handleChange} />
-              </div>
-              <div className="task-cell">
-                <select id="sprint" onChange={handleChange} value={task.sprint}>
-                  <option value=""> </option>
-                  {sprints.map((sprint, index) => (
-                    <option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
-                </select>
-              </div>
-              <div className="task-cell">
-                <select id="tag" onChange={handleChange} value={task.tag}>
-                  {COLORS.map((color) => (
-                    <option value={color.tag} key={color.tag}>{color.name}</option>))}
-                </select>
-              </div>
-              <div className="task-cell">
-                <button type="button" onClick={handleDelete}>削除</button>
-              </div>
+        <table>
+          <thead className="header">
+            <tr>
+              <th width="50%">タスク名</th>
+              <th width="15%">ポイント</th>
+              <th width="15%">完了</th>
+              <th width="15%">色</th>
+              <th width="auto"> </th>
+            </tr>
+          </thead>
+          {/* <div className="app-table-header">
+            <div className="task-cell">
+              タスク名
             </div>
-          ))}
-        </div>
-        <div className="app-table-footer">
-          <button type="button" name="add-button" onClick={handleClick}>タスクを追加</button>
-        </div>
+            <div className="task-cell">
+              ポイント
+            </div>
+            <div className="task-cell">
+              完了Sprint
+            </div>
+            <div className="task-cell">
+              色
+            </div>
+            <div className="task-cell"> </div>
+          </div> */}
+          {/* <div className="app-table-body"> */}
+          <tbody>
+            {tasks.map((task) => (
+              <tr id={task.id} key={task.id}>
+                <td>
+                  <input type="text" id="title" value={task.title} onChange={handleChange} />
+                </td>
+                <td>
+                  <input type="number" id="point" value={task.point} onChange={handleChange} />
+                </td>
+                <td>
+                  <select id="sprint" onChange={handleChange} value={task.sprint}>
+                    <option value=""> </option>
+                    {sprints.map((sprint, index) => (
+                      <option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
+                  </select>
+                </td>
+                <td>
+                  <select id="tag" onChange={handleChange} value={task.tag}>
+                    {COLORS.map((color) => (
+                      <option value={color.tag} key={color.tag}>{color.name}</option>))}
+                  </select>
+                </td>
+                <td>
+                  <button type="button" onClick={handleDelete}>削除</button>
+                </td>
+              </tr>
+              // <div draggable="true" className="app-table-row" id={task.id} key={task.id}>
+              //   <div className="task-name-cell">
+              //     <input type="text" id="title" value={task.title} onChange={handleChange} />
+              //   </div>
+              //   <div className="task-point-cell">
+              //     <input type="number" id="point" value={task.point} onChange={handleChange} />
+              //   </div>
+              //   <div className="task-cell">
+              //     <select id="sprint" onChange={handleChange} value={task.sprint}>
+              //       <option value=""> </option>
+              //       {sprints.map((sprint, index) => (
+              //         <option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
+              //     </select>
+              //   </div>
+              //   <div className="task-cell">
+              //     <select id="tag" onChange={handleChange} value={task.tag}>
+              //       {COLORS.map((color) => (
+              //         <option value={color.tag} key={color.tag}>{color.name}</option>))}
+              //     </select>
+              //   </div>
+              //   <div className="task-cell">
+              //     <button type="button" onClick={handleDelete}>削除</button>
+              //   </div>
+              // </div>
+            ))}
+            {/* </div> */}
+          </tbody>
+        </table>
+      </div>
+      <div className="app-table-footer">
+        <button type="button" name="add-button" onClick={handleClick}>タスクを追加</button>
       </div>
     </>
   );
