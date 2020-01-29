db497c0d066918880d9dfb1ace48ec4b5e037e37
m-daichi1219 <matu9525@gmail.com>
2020-01-29 21:38:44 +0900

    Add task edit form for modal view

--- src/component/AppModalView.js
+++ src/component/AppModalView.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { EDIT_TASK, DELETE_TASK, SET_CURRENT_TASK } from '../constants/action-types';
+import COLORS from '../constants/tag-colors';
 
 const AppModalView = () => {
   const task = useSelector((state) => state.currentTask);
+  const sprints = useSelector((state) => state.sprints);
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const payload = {};
+    payload.id = event.target.parentNode.id;
+    payload[event.target.id] = event.target.value;
+
+    dispatch({ type: EDIT_TASK, payload });
+    dispatch({ type: SET_CURRENT_TASK, task });
+  };
+
+  const handleDelete = (event) => {
+    const payload = {};
+    payload.id = event.target.parentNode.id;
+    dispatch({ type: DELETE_TASK, payload });
+  };
+
   return (
-    <>
-      <p>AppModalView</p>
-      <p>{task.id}</p>
-    </>
+    <div id={task.id}>
+      <input type="text" id="title" value={task.title} onChange={handleChange} />
+      <input type="number" id="point" value={task.point} onChange={handleChange} />
+      <select id="sprint" onChange={handleChange} value={task.sprint}>
+        <option value=""> </option>
+        {sprints.map((sprint, index) => (
+          <option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
+      </select>
+      <select id="tag" onChange={handleChange} value={task.tag}>
+        {COLORS.map((color) => (
+          <option value={color.tag} key={color.tag}>{color.name}</option>))}
+      </select>
+      <button type="button" onClick={handleDelete}>削除</button>
+    </div>
   );
 };
 export default AppModalView;
