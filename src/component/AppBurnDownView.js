0677695f947f8c987c2e995bc2431fbbe57c1dac
m-daichi1219 <matu9525@gmail.com>
2020-02-11 14:45:50 +0900

    Fix layout and css

--- src/component/AppBurnDownView.js
+++ src/component/AppBurnDownView.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import AppTaskTable from './AppTaskTable';
 import AppSprintTable from './AppSprintTable';
 import AppLineChart from './AppLineChart';
 import '../css/AppBurnDownView.css';
 
 const AppBurnDownView = () => (
   <div className="burndown-layout">
-    <div className="task-area">
-      <AppTaskTable />
-    </div>
     <div className="chart-area">
       <AppLineChart />
     </div>
