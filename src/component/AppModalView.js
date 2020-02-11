43a40a31ab618b9d0acc326ab71b18a7bd447f23
m-daichi1219 <matu9525@gmail.com>
2020-02-11 14:48:12 +0900

    Add detail area and layout update

--- src/component/AppModalView.js
+++ src/component/AppModalView.js
@@ -3,10 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { EDIT_TASK } from '../constants/action-types';
 import COLORS from '../constants/tag-colors';
+import '../css/AppModalView.css';
 
 const AppModalView = (props) => {
   const [del, setDelete] = useState(false);
-  const { deleteModal } = props;
+  const { deleteModal, closeModal } = props;
   const currentNum = useSelector((state) => state.currentTask);
   const task = useSelector((state) => state.datas[currentNum]);
   const sprints = useSelector((state) => state.sprints);
@@ -14,7 +15,7 @@ const AppModalView = (props) => {
 
   const handleChange = (event) => {
     const payload = {};
-    payload.id = event.target.parentNode.id;
+    payload.id = event.target.parentNode.parentNode.id;
     payload[event.target.id] = event.target.value;
 
     dispatch({ type: EDIT_TASK, payload });
@@ -27,22 +28,39 @@ const AppModalView = (props) => {
       deleteModal(payload);
       setDelete(false);
     }
-  });
+  }, [del, task.id, deleteModal]);
 
   return (
     <>
-      <div id={task.id}>
-        <input type="text" id="title" value={task.title} onChange={handleChange} />
-        <input type="number" id="point" value={task.point} onChange={handleChange} />
-        <select id="sprint" onChange={handleChange} value={task.sprint}>
-          <option value=""> </option>
-          {sprints.map((sprint, index) => (
-            <option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
-        </select>
-        <select id="tag" onChange={handleChange} value={task.tag}>
-          {COLORS.map((color) => (
-            <option value={color.tag} key={color.tag}>{color.name}</option>))}
-        </select>
+      <div id={task.id} className="modal-style">
+        <button type="button" className="modal-close-button" onClick={closeModal}> </button>
+        <label htmlFor="title" className="modal-label-style">
+          TITLE
+          <input type="text" id="title" value={task.title} onChange={handleChange} />
+        </label>
+        <label htmlFor="detail" className="modal-label-style">
+          DETAIL
+          <textarea type="text" id="detail" value={task.detail} onChange={handleChange} />
+        </label>
+        <label htmlFor="point" className="modal-label-style">
+          POINT
+          <input type="number" id="point" value={task.point} onChange={handleChange} />
+        </label>
+        <label htmlFor="sprint" className="modal-label-style">
+          SPRINT
+          <select id="sprint" onChange={handleChange} value={task.sprint}>
+            <option value=""> </option>
+            {sprints.map((sprint, index) => (
+              <option value={index + 1} key={sprint.id}>{`Sprint${index + 1}`}</option>))}
+          </select>
+        </label>
+        <label htmlFor="sprint" className="modal-label-style">
+          COLOR
+          <select id="tag" onChange={handleChange} value={task.tag}>
+            {COLORS.map((color) => (
+              <option value={color.tag} key={color.tag}>{color.name}</option>))}
+          </select>
+        </label>
         <button type="button" onClick={() => setDelete(true)}>削除</button>
       </div>
     </>
@@ -51,6 +69,7 @@ const AppModalView = (props) => {
 
 AppModalView.propTypes = {
   deleteModal: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
 };
 
 export default AppModalView;
